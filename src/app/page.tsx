757f48d918e6e1bce38693ab41b0650cc600c134
YASH <46160689+theyashjaiswal@users.noreply.github.com>
2024-09-21 22:06:40 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -480,12 +480,12 @@ export default function Page() {
                 <BlurFade delay={BLUR_FADE_DELAY}>
                   <div className="flex flex-row flex-wrap gap-1">
                     <p>Software Engineer 👨🏽‍💻 </p>
-                   // <p>{" | "}</p>
-                  //<p>Music Artist 🎙️🎹🎵</p>
+                   {/*<p>{" | "}</p>
+                  <p>Music Artist 🎙️🎹🎵</p>*/}
                   </div>
                   <div className="flex flex-row flex-wrap gap-1">
                     <p>📚 CS @ Dal 🐯</p>
-                    <p> | </p>
+                   {/* <p> | </p>*/}<br/>
                     <p>📍 🇮🇳 🇨🇦</p>
                   </div>
                   {/* <div className="flex flex-row flex-wrap gap-1 pt-2">
